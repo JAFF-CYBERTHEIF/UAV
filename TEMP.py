0c13cea27fc7610e3ff716f8c423fa3e400ce385
JAWAHAR <63846848+JAFF-CYBERTHEIF@users.noreply.github.com>
2023-03-09 15:47:11 +0530

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -1,16 +1,16 @@
-import cv2
-import numpy as np
 import socket
-import sys
-import pickle
-import struct
 
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8089))
+HOST = " 169.254.215.204"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-cap=cv2.VideoCapture(0)
-while True:
-    ret,frame=cap.read()
-    data = pickle.dumps(frame)
-    message_size = struct.pack("L", len(data))
-    clientsocket.sendall(message_size + data)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
