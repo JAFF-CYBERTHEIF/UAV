86ad7dd2446a28adf601fbb2b87d67d1f929f646
JAWAHAR <63846848+JAFF-CYBERTHEIF@users.noreply.github.com>
2023-04-05 12:00:48 +0530

    Update GPS.py

--- GPS.py
+++ GPS.py
@@ -2,7 +2,7 @@ import serial
 
 ser = serial.Serial("/dev/ttyAMA0" ,baudrate=9600)
 
-def read_GPS:
+def read_GPS():
   GPS = ser.readline().decode().strip().split(",")
   #returns a list [Time, date, lat, long, speed, course]
   return GPS
